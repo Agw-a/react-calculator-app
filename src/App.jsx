76961713bd0ee9d09e04813db2948df5f5b61490
Agw-a <lizagwa7@gmail.com>
2022-10-07 14:51:25 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import Output from './components/output'
 import Button from './components/button'
 import ButtonWrap from './components/buttonWrap'
 
+const buttonValues = [
+  ['C', '%', '<-', '/'],
+  ['7', '8', '9', 'X'],
+  ['4','5',, '6', '-'],
+  ['1', '2', '3', '+'],
+  ['00', '0', '.', '=']
+];
 
 export default function App() {
 
