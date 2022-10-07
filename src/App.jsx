7199126e31dd6fd85991cca0998f909730045cae
Agw-a <lizagwa7@gmail.com>
2022-10-07 15:03:42 +0300

    restarts

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,11 @@
-import './App.css'
-import MainWrapper from './components/MainWrapper'
-import Output from './components/output'
-import Button from './components/button'
-import ButtonWrap from './components/buttonWrap'
+import React from 'react'
 
-const buttonValues = [
-  ['C', '%', '<-', '/'],
-  ['7', '8', '9', 'X'],
-  ['4','5',, '6', '-'],
-  ['1', '2', '3', '+'],
-  ['00', '0', '.', '=']
-];
-
-export default function App() {
-
-  const handleOnclick = () => {
-    console.log(`${btn} clicked`)
-  }
+const App = () => {
   return (
-    <MainWrapper>
-      <Output value = '0'/>
-      <ButtonWrap>
-        {
-          buttonValues.flat().map((btn, i) => {
-            return(
-              <Button
-              key={i}
-              className={btn === '=' ? 'equals' : ''}
-              value={btn}
-              onClick={handleOnclick}
-              />
-            )
-          })
-        }
-      </ButtonWrap>
-        React ⚛️ + Vite ⚡ + Replit 🌀
-    </MainWrapper>
+    <div>
       
-    
-    
+    </div>
   )
 }
+
+export default App
