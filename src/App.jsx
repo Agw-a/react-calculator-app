c3803bf28de3cde08ed15da502847054c5faf17e
Agw-a <lizagwa7@gmail.com>
2022-10-07 15:09:02 +0300

    Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
+    <div className='App' >
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span> 0
+        </div>
+
+        <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+
+          <button>DEL</button>
+        </div>
+
+        <div className="digits">
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
       
     </div>
   )
