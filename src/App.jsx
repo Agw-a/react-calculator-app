df367f972488b81ab004dfaea6a8aff824ad0250
A-gwa <89134520+Agw-a@users.noreply.github.com>
2022-10-12 12:19:09 +0000

    New file

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Calculator from './components/calculator/calculator'
+export default function App() {
+  return (
+    <main>
+    <Calculator/>
+     
+    </main>
+  )
+}
